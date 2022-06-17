32eec8d5391a47f559226db682ac389793752882
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-06-17 08:10:55 +0300

    pre Task 25

---
+++ Task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+
+*/
+
+int InputNum(string msg){
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = InputNum("INput number: ");
